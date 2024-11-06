6b4d12d4ca882d17d928e291f1738f0ac2940e90
Paula-Munoz-Teno <paulamtlaboral@gmail.com>
2024-11-06 15:41:41 +0100

    subida retos dia1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { ComponentsComponent } from './components/components.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { FormRegisterComponent } from './components/form-register/form-register.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    RegisterComponent,
+    ComponentsComponent,
+    HeaderComponent,
+    FooterComponent,
+    FormRegisterComponent,
+   
   ],
   imports: [
     BrowserModule,
